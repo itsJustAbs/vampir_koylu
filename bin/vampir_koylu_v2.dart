a4875e40fbbc8a1e79fd572807d6b27c4d054454
itsJustAbs <ardabaransaral6@gmail.com>
2024-03-25 22:57:59 +0300

    Added player ranges so I can set a player range dependeing on the amount of players :)

--- bin/vampir_koylu_v2.dart
+++ bin/vampir_koylu_v2.dart
@@ -15,12 +15,21 @@ void main() {
 Map<String, Roles> userRoles = {};
 
 
-// Needed roles
+// Needed roles for 4-6 player range
 List<Roles> rolesFor4to6players = [
-    Roles.vampir,
     Roles.vampir,
     Roles.doktor,
 ];
+
+// Needed roles for 7-10 player range
+List<Roles> rolesFor7to10players = [
+Roles.vampir,
+Roles.vampir,
+Roles.doktor,
+Roles.mafya,
+];
+
+if(users.length <= 6){
 
 // For every role in the role list gets a random person to have that role and deletes the user
 for(var role in rolesFor4to6players){
@@ -34,7 +43,22 @@ for (var user in users) {
     userRoles[user] = Roles.koylu;
 }
 
+}
 
+if(users.length >= 7){
+
+// For every role in the role list gets a random person to have that role and deletes the user
+for(var role in rolesFor7to10players){
+  String randomUser = getRandomName();
+  userRoles[randomUser] = role;
+  users.remove(randomUser);
+}
+
+// For every user left in users gets a random role and assigns it to them
+for (var user in users) {
+    userRoles[user] = Roles.koylu;
+}
+}
 print(userRoles);
 }
 
@@ -53,4 +77,5 @@ String getRandomName() {
 
 
 // Test users over here
-List<String> users = ['arda','kemal','mehmet','ahmet','ayse','ali'];
+List<String> users = ['arda','kemal','mehmet','ahmet','ayse','ali','muhammet'];
+// Note: you can try removeing some of the users to see the amount of special roles given change 
